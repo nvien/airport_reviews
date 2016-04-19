48ac735e6cbb110598aeb3f8169b993701323be1
Ngan <Katie@Ngans-MacBook-Pro.local>
2016-04-19 14:39:43 -0400

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :reviews, dependent: :destroy #To delete all reviews associated with a particular user when that user account is deleted
 end
