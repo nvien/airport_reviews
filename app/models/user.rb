444bfce9149a94ebf4932cf6dc6dacb98cded2c4
Ngan <Katie@Ngans-MacBook-Pro.local>
2016-04-23 17:25:24 -0400

    Add admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews, dependent: :destroy #To delete all reviews associated with a particular user when that user account is deleted
+  # def admin?
+  #  role == "admin"
+  # end
 end
